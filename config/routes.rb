55463c698b2ddb0ab944a369c0434292a697dee5
Rise Nguyen <hnguyen33@rrc.ca>
2025-10-27 09:22:56 -0500

    Add customer views and actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root to: "admin/dashboard#index"
+  root to: "customers#index"
+  get "customers/alphabetized"
+  get "customers/missing_email"
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
